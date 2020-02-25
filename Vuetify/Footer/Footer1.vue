92a535ba94380146a229df08a1ae4ea6e88b5d19
SacleuxBenoit <benoitsacleux@outlook.fr>
2020-02-25 23:36:25 +0100

    add content in the file Footer1.vue

---
+++ Vuetify/Footer/Footer1.vue
@@ -0,0 +1,16 @@
+<template>
+  <v-footer padless>
+    <v-col
+      cols="12"
+      class="center"
+    >
+   © Copyright 2019-{{ new Date().getFullYear() }} - Tous droits réservés. Reproduction totale ou partielle interdite sans l'autorisation de l'auteur.
+    </v-col>
+  </v-footer>
+</template>
+
+<style>
+  .center{
+    text-align: center;
+  }
+</style>
